cacdfa33bd85a4e697dfc36216008a50a43c08fb
Tenzin Disaro <tengobilt@gmail.com>
2024-06-19 05:27:02 -0300

    base html css feita, integration crriando bd firebase

---
+++ Desktop/site_de_eventos/assets/js/main.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAf-VZICZtPi5qZRTeTiK7Dj7nEhNFeHuY",
+    authDomain: "site-de-eventos-764bc.firebaseapp.com",
+    projectId: "site-de-eventos-764bc",
+    storageBucket: "site-de-eventos-764bc.appspot.com",
+    messagingSenderId: "1034878134560",
+    appId: "1:1034878134560:web:2e70f7ad5bb69258248f45"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
